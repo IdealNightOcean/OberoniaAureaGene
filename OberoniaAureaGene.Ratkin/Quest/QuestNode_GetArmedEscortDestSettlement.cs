33628ef0c889efb1baeae6fdf4a3e9f9cb594abc
“IdealNightOcean” <idealyehai@qq.com>
2024-08-18 19:03:17 +0800

    获取目标押送点

---
+++ OberoniaAureaGene.Ratkin/Quest/QuestNode_GetArmedEscortDestSettlement.cs
@@ -0,0 +1,31 @@
+﻿using OberoniaAurea_Frame;
+using RimWorld.Planet;
+using RimWorld.QuestGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace OberoniaAureaGene.Ratkin;
+
+public class QuestNode_GetArmedEscortDestSettlement : QuestNode_GetNearbySettlementOfFaction
+{
+    public SlateRef<WorldObject> startSettlement;
+    public SlateRef<int> minEscortTileDistance;
+
+    protected override bool ValidSettlement(Settlement settlement, Slate slate)
+    {
+        int originTile = startSettlement.GetValue(slate)?.Tile ?? -1;
+        if (originTile < 0)
+        {
+            return false;
+        }
+        if (Find.WorldGrid.ApproxDistanceInTiles(originTile, settlement.Tile) < minEscortTileDistance.GetValue(slate))
+        {
+            return false;
+        }
+        return true;
+    }
+}
