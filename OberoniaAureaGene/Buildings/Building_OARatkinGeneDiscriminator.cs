15f6648ea61b7936124d0db6860775115f86c356
“IdealNightOcean” <idealyehai@qq.com>
2024-08-21 19:14:02 +0800

    鸢尾兰基因鉴别器修正

--- OberoniaAureaGene/Buildings/Building_OARatkinGeneDiscriminator.cs
+++ OberoniaAureaGene/Buildings/Building_OARatkinGeneDiscriminator.cs
@@ -1,14 +1,12 @@
 ﻿using RimWorld;
-using System.Linq;
-using UnityEngine;
 using Verse;
 
 namespace OberoniaAureaGene;
 
 [StaticConstructorOnStartup]
-public class Building_OARatkinGeneDiscriminator : Building_GeneExtractorBase
+public class Building_OARatkinGeneDiscriminator : Building_GeneDiscriminatorBase
 {
-    protected override int TicksToExtract => 130000;
+    protected override int TicksToDiscriminat => 180000;
     protected static readonly SimpleCurve FailChanceCurve =
     [
         new CurvePoint(1f, 0f),
@@ -17,43 +15,27 @@ public class Building_OARatkinGeneDiscriminator : Building_GeneExtractorBase
         new CurvePoint(4f, 0.55f)
     ];
 
-    public override void PostMake()
-    {
-        base.PostMake();
-    }
-
-    public override void TryStartWork(Pawn pawn)
-    {
-        Find.WindowStack.Add(new Dialog_ExtractGenes(pawn, this));
-    }
     protected override void FinishWork()
     {
         Map map = base.Map;
-        Pawn containedPawn = ContainedPawn;
+        Genepack containedGenepack = ContainedGenepack;
         bool success = false;
-        float faliChance = FailChanceCurve.Evaluate(targetGenes.Count);
+        float faliChance = FailChanceCurve.Evaluate(containedGenepack.GeneSet.GenesListForReading.Count);
         if (!Rand.Chance(faliChance))
         {
             success = true;
-            foreach (GeneDef gene in targetGenes)
-            {
-                Genepack genepack = (Genepack)ThingMaker.MakeThing(ThingDefOf.Genepack);
-                genepack.Initialize([gene]);
-                GenPlace.TryPlaceThing(genepack, placePos, map, ThingPlaceMode.Near);
-            }
-        }
-        GeneUtility.ExtractXenogerm(containedPawn, Mathf.RoundToInt(60000f * GeneTuning.GeneExtractorRegrowingDurationDaysRange.RandomInRange));
-        if (!containedPawn.Dead && (containedPawn.IsPrisonerOfColony || containedPawn.IsSlaveOfColony))
-        {
-            containedPawn.needs?.mood?.thoughts?.memories?.TryGainMemory(ThoughtDefOf.XenogermHarvested_Prisoner);
+            Genepack genepack = (Genepack)ThingMaker.MakeThing(ThingDefOf.Genepack);
+            genepack.Initialize([targetGeneDef]);
+            GenPlace.TryPlaceThing(genepack, placePos, map, ThingPlaceMode.Near);
+
         }
         if (success)
         {
-            Messages.Message("GeneExtractionComplete".Translate(containedPawn.Named("PAWN")) + ": " + targetGenes.Select((GeneDef x) => x.label).ToCommaList().CapitalizeFirst(), containedPawn, MessageTypeDefOf.PositiveEvent);
+            Messages.Message("OAGene_GeneDiscriminationComplete".Translate(targetGeneDef.label).CapitalizeFirst(), targetGenepack, MessageTypeDefOf.PositiveEvent);
         }
         else
         {
-            Messages.Message("OAGene_GeneExtractionFail".Translate(containedPawn.Named("PAWN")).CapitalizeFirst(), new LookTargets(containedPawn), MessageTypeDefOf.NegativeEvent);
+            Messages.Message("OAGene_GeneDiscriminationFail".Translate(targetGeneDef.label).CapitalizeFirst(), targetGenepack, MessageTypeDefOf.NegativeEvent);
         }
         base.FinishWork();
     }
