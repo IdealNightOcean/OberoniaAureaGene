0413db54d7c582faffd06118b4c0b6a1e57c8dc1
IdealNightOcean <idealyehai@qq.com>
2026-02-27 21:30:45 +0800

    26.02.28 尝试修复归乡任务触发的卡顿

--- OberoniaAureaGene.Snowstorm/Utility/DefOf.cs
+++ OberoniaAureaGene.Snowstorm/Utility/DefOf.cs
@@ -7,36 +7,89 @@ namespace OberoniaAureaGene.Snowstorm;
 [DefOf]
 public static class Snowstorm_MiscDefOf
 {
-    public static DamageDef OAGene_IceStab; //冰晶刺伤
+    /// <summary>
+    /// 冰晶刺伤
+    /// </summary>
+    public static DamageDef OAGene_IceStab;
 
-    public static FleckDef OAGene_StarryGlow; //星月夜特效
+    /// <summary>
+    /// 星月夜特效
+    /// </summary>
+    public static FleckDef OAGene_StarryGlow;
 
     public static IsolatedPawnGroupMakerDef OAGene_GroupMaker_SnowstormCultist;
 
-    public static GameConditionDef OAGene_SnowstormPrecursor; //气温骤降
-    public static GameConditionDef OAGene_SnowyCrystalTreeCooler; //风晶雪树的降温
-    public static GameConditionDef OAGene_EndGame_ExtremeSnowstorm; //终局风雪
+    /// <summary>
+    /// 气温骤降
+    /// </summary>
+    public static GameConditionDef OAGene_SnowstormPrecursor;
+    /// <summary>
+    /// 风晶雪树的降温
+    /// </summary>
+    public static GameConditionDef OAGene_SnowyCrystalTreeCooler;
+    /// <summary>
+    /// 终局风雪
+    /// </summary>
+    public static GameConditionDef OAGene_EndGame_ExtremeSnowstorm;
 
     public static JobDef OAGene_Job_TakeIceCrystalOutOfCollector;
 
-    public static MentalBreakDef OAGene_LostInMemory; //陷入回忆
+    /// <summary>
+    /// 主角状态：陷入回忆
+    /// </summary>
+    public static MentalBreakDef OAGene_LostInMemory;
 
-    public static MusicTransitionDef OAGene_Transition_StarryNight; //星月夜BGM
-    public static MusicTransitionDef OAGene_Transition_Liebestraum; //终局风雪BGM
+    /// <summary>
+    /// 星月夜BGM
+    /// </summary>
+    public static MusicTransitionDef OAGene_Transition_StarryNight;
+    /// <summary>
+    /// 终局风雪BGM
+    /// </summary>
+    public static MusicTransitionDef OAGene_Transition_Liebestraum;
 
-    public static RaidStrategyDef OAGene_SnowstormImmediateAttackBreaching; //暴风雪破墙袭击
-    public static RaidStrategyDef OAGene_ImmediateAttack_SnowstormCultist; //教徒死战不退袭击
+    /// <summary>
+    /// 终局任务：长路归乡
+    /// </summary>
+    public static QuestScriptDef OAGene_EndGame_Homecoming;
 
-    public static SongDef OAGene_IGiorni; //终局BGM
+    /// <summary>
+    /// 暴风雪破墙袭击
+    /// </summary>
+    public static RaidStrategyDef OAGene_SnowstormImmediateAttackBreaching;
+    /// <summary>
+    /// 教徒死战不退袭击
+    /// </summary>
+    public static RaidStrategyDef OAGene_ImmediateAttack_SnowstormCultist;
+
+    /// <summary>
+    /// 终局BGM
+    /// </summary>
+    public static SongDef OAGene_IGiorni;
 
     public static TraderKindDef OAGene_Trader_SnowstormCamp;
 
-    public static WeatherDef OAGene_SnowExtreme; //极端暴风雪
-    public static WeatherDef OAGene_IceSnowExtreme; //冰晶暴风雪
-    public static WeatherDef OAGene_IceRain; //冰晶雨
+    /// <summary>
+    /// 极端暴风雪
+    /// </summary>
+    public static WeatherDef OAGene_SnowExtreme;
+    /// <summary>
+    /// 冰晶暴风雪
+    /// </summary>
+    public static WeatherDef OAGene_IceSnowExtreme;
+    /// <summary>
+    /// 冰晶雨
+    /// </summary>
+    public static WeatherDef OAGene_IceRain;
 
-    public static WorldObjectDef OAGene_Hometown; //家乡
-    public static WorldObjectDef OAGene_Hometown_Sealed; //家乡（封存占格子）
+    /// <summary>
+    /// 家乡
+    /// </summary>
+    public static WorldObjectDef OAGene_Hometown;
+    /// <summary>
+    /// 家乡（封存状态）
+    /// </summary>
+    public static WorldObjectDef OAGene_Hometown_Sealed;
     static Snowstorm_MiscDefOf()
     {
         DefOfHelper.EnsureInitializedInCtor(typeof(Snowstorm_MiscDefOf));
@@ -46,11 +99,26 @@ public static class Snowstorm_MiscDefOf
 [DefOf]
 public static class Snowstorm_ThingDefOf
 {
-    public static ThingDef OAGene_IceCrystal; //风雪碎晶
-    public static ThingDef OAGene_IceCrystalCollector; //风雪碎晶收集器
-    public static ThingDef OAGene_Plant_IceCrystalFlower; //碎晶花
-    public static ThingDef OAGene_AntiSnowTorch; //风雪火把
-    public static ThingDef OAGene_Plant_SnowyCrystalTree_Seed; //风雪树种
+    /// <summary>
+    /// 风雪碎晶
+    /// </summary>
+    public static ThingDef OAGene_IceCrystal;
+    /// <summary>
+    /// 风雪碎晶收集器
+    /// </summary>
+    public static ThingDef OAGene_IceCrystalCollector;
+    /// <summary>
+    /// 碎晶花
+    /// </summary>
+    public static ThingDef OAGene_Plant_IceCrystalFlower;
+    /// <summary>
+    /// 风雪火把
+    /// </summary>
+    public static ThingDef OAGene_AntiSnowTorch;
+    /// <summary>
+    /// 风雪树种
+    /// </summary>
+    public static ThingDef OAGene_Plant_SnowyCrystalTree_Seed;
     static Snowstorm_ThingDefOf()
     {
         DefOfHelper.EnsureInitializedInCtor(typeof(Snowstorm_ThingDefOf));
@@ -61,23 +129,62 @@ public static class Snowstorm_ThingDefOf
 [DefOf]
 public static class Snowstorm_HediffDefOf
 {
-    public static HediffDef OAGene_Hediff_ProtagonistHomecoming; //主角归乡健康状态（用于心情）
-    public static HediffDef OAGene_Hediff_ProtagonistHomecomed; //主角回忆健康状态（用于机制)
+    /// <summary>
+    /// 主角归乡健康状态（用于心情）
+    /// </summary>
+    public static HediffDef OAGene_Hediff_ProtagonistHomecoming;
+    /// <summary>
+    /// 主角回忆健康状态（用于机制)
+    /// </summary>
+    public static HediffDef OAGene_Hediff_ProtagonistHomecomed;
 
-    public static HediffDef OAGene_Hediff_ColdPreparation_Enemy; //充足御寒准备（敌人）
-    public static HediffDef OAGene_Hediff_ColdPreparation_Neutral; //充足御寒准备（中立）
-    public static HediffDef OAGene_Hediff_HopeForSurvival; //求生的希望
-    public static HediffDef OAGene_Hediff_ExperienceSnowstorm; //经历暴风雪
-    public static HediffDef OAGene_Hediff_HideInSnowstorm; //隐匿于风雪
+    /// <summary>
+    /// 充足御寒准备（敌人）
+    /// </summary>
+    public static HediffDef OAGene_Hediff_ColdPreparation_Enemy;
+    /// <summary>
+    /// 充足御寒准备（中立）
+    /// </summary>
+    public static HediffDef OAGene_Hediff_ColdPreparation_Neutral;
+    /// <summary>
+    /// 求生的希望
+    /// </summary>
+    public static HediffDef OAGene_Hediff_HopeForSurvival;
+    /// <summary>
+    /// 经历暴风雪
+    /// </summary>
+    public static HediffDef OAGene_Hediff_ExperienceSnowstorm;
+    /// <summary>
+    /// 隐匿于风雪
+    /// </summary>
+    public static HediffDef OAGene_Hediff_HideInSnowstorm;
 
-    public static HediffDef OAGene_Hediff_SnowstormOblivious; //忘我
-    public static HediffDef OAGene_Hediff_SnowstormAngry; //不理想的愤怒
+    /// <summary>
+    /// 忘我
+    /// </summary>
+    public static HediffDef OAGene_Hediff_SnowstormOblivious;
+    /// <summary>
+    /// 不理想的愤怒
+    /// </summary>
+    public static HediffDef OAGene_Hediff_SnowstormAngry;
 
-    public static HediffDef OAGene_Hediff_SnowstormCultist; //风雪教徒健康状态
-    public static HediffDef OAGene_Hediff_SnowstormStrugglers; //难民健康状态（用于心情）
-    public static HediffDef OAGene_Hediff_SpecialThrumbo; //特殊敲击兽
+    /// <summary>
+    /// 风雪教徒健康状态
+    /// </summary>
+    public static HediffDef OAGene_Hediff_SnowstormCultist;
+    /// <summary>
+    /// 难民健康状态（用于心情）
+    /// </summary>
+    public static HediffDef OAGene_Hediff_SnowstormStrugglers;
+    /// <summary>
+    /// 特殊敲击兽
+    /// </summary>
+    public static HediffDef OAGene_Hediff_SpecialThrumbo;
 
-    public static HediffDef OAGene_Hediff_IceCrystalFlowerSea; //冰晶花海
+    /// <summary>
+    /// 冰晶花海
+    /// </summary>
+    public static HediffDef OAGene_Hediff_IceCrystalFlowerSea;
     static Snowstorm_HediffDefOf()
     {
         DefOfHelper.EnsureInitializedInCtor(typeof(Snowstorm_HediffDefOf));
@@ -88,15 +195,33 @@ public static class Snowstorm_HediffDefOf
 [DefOf]
 public static class Snowstorm_ThoughtDefOf
 {
-    public static ThoughtDef OAGene_Thought_ProtagonistHomecoming; //主角归乡心情
+    /// <summary>
+    /// 主角归乡心情
+    /// </summary>
+    public static ThoughtDef OAGene_Thought_ProtagonistHomecoming;
 
-    public static ThoughtDef OAGene_Thought_SnowstormEnd; //暴风雪结束心情
-    public static ThoughtDef OAGene_Thought_StarryNightP; //星月夜主角心情
-    public static ThoughtDef OAGene_Thought_SnowstormStrugglers; //难民心情
+    /// <summary>
+    /// 暴风雪结束心情
+    /// </summary>
+    public static ThoughtDef OAGene_Thought_SnowstormEnd;
+    /// <summary>
+    /// 星月夜主角心情
+    /// </summary>
+    public static ThoughtDef OAGene_Thought_StarryNightP;
+    /// <summary>
+    /// 难民心情
+    /// </summary>
+    public static ThoughtDef OAGene_Thought_SnowstormStrugglers;
 
-    public static ThoughtDef OAGene_Thought_SnowstormCultistConvert; //难民心情
+    /// <summary>
+    /// 难民心情
+    /// </summary>
+    public static ThoughtDef OAGene_Thought_SnowstormCultistConvert;
 
-    public static ThoughtDef OAGene_Thought_IceCrystalFlowerSea; //冰晶花海心情
+    /// <summary>
+    /// 冰晶花海心情
+    /// </summary>
+    public static ThoughtDef OAGene_Thought_IceCrystalFlowerSea;
     static Snowstorm_ThoughtDefOf()
     {
         DefOfHelper.EnsureInitializedInCtor(typeof(Snowstorm_ThoughtDefOf));
@@ -107,32 +232,92 @@ public static class Snowstorm_ThoughtDefOf
 [DefOf]
 public static class Snowstorm_IncidentDefOf
 {
-    public static IncidentDef OAGene_EndGame_ExtremeSnowstorm; //终局风雪
+    /// <summary>
+    /// 终局风雪
+    /// </summary>
+    public static IncidentDef OAGene_EndGame_ExtremeSnowstorm;
 
-    public static IncidentDef OAGene_ExtremeSnowstorm; //极端暴风雪
-    public static IncidentDef OAGene_StarryNight; //星月夜
-    public static IncidentDef OAGene_SnowstormFog; //雪雾弥漫
+    /// <summary>
+    /// 极端暴风雪
+    /// </summary>
+    public static IncidentDef OAGene_ExtremeSnowstorm;
+    /// <summary>
+    /// 星月夜
+    /// </summary>
+    public static IncidentDef OAGene_StarryNight;
+    /// <summary>
+    /// 雪雾弥漫
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormFog;
 
-    public static IncidentDef OAGene_ExtremeIceStorm; //冰晶暴风雪
-    public static IncidentDef OAGene_SnowstormWarm; //暴风雪的暖和
-    public static IncidentDef OAGene_SnowstormCold; //暴风雪的骤冷
+    /// <summary>
+    /// 冰晶暴风雪
+    /// </summary>
+    public static IncidentDef OAGene_ExtremeIceStorm;
+    /// <summary>
+    /// 暴风雪的暖和
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormWarm;
+    /// <summary>
+    /// 暴风雪的骤冷
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormCold;
 
-    public static IncidentDef OAGene_SnowstormMaliceRaid; //暴风雪破墙袭击
-    public static IncidentDef OAGene_SnowstormMaliceRaid_Reinforce; //暴风雪破墙袭击（加强）
-    public static IncidentDef OAGene_SnowstormMaliceRaid_Hard; //暴风雪破墙袭击（困难）
-    public static IncidentDef OAGene_SnowstormRaidSource; //暴风雪中的恶意（袭击）
+    /// <summary>
+    /// 暴风雪破墙袭击
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormMaliceRaid;
+    /// <summary>
+    /// 暴风雪破墙袭击（加强）
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormMaliceRaid_Reinforce;
+    /// <summary>
+    /// 暴风雪破墙袭击（困难）
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormMaliceRaid_Hard;
+    /// <summary>
+    /// 暴风雪中的恶意（袭击）
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormRaidSource;
     [MayRequireRoyalty]
-    public static IncidentDef OAGene_SnowstormClimateAdjuster; //暴风雪中的恶意（气候）
-    public static IncidentDef OAGene_SnowstormCultistRaid; //暴风雪狂热教徒袭击
+    /// <summary>
+    /// 暴风雪中的恶意（气候）
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormClimateAdjuster;
+    /// <summary>
+    /// 暴风雪狂热教徒袭击
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormCultistRaid;
 
-    public static IncidentDef OAGene_SnowstormStrugglers; //暴风雪中的挣扎者
-    public static IncidentDef OAGene_AffectedMerchant; //暴风雪中的遇难商人
-    public static IncidentDef OAGene_SnowstormThrumboWanderIn; //暴风雪中的敲击兽
-    public static IncidentDef OAGene_CommunicationTowerCollapse; //通讯塔倒塌
-    public static IncidentDef OAGene_SnowstormPrecursor_AnimalFlee; //动物逃离
+    /// <summary>
+    /// 暴风雪中的挣扎者
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormStrugglers;
+    /// <summary>
+    /// 暴风雪中的遇难商人
+    /// </summary>
+    public static IncidentDef OAGene_AffectedMerchant;
+    /// <summary>
+    /// 暴风雪中的敲击兽
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormThrumboWanderIn;
+    /// <summary>
+    /// 通讯塔倒塌
+    /// </summary>
+    public static IncidentDef OAGene_CommunicationTowerCollapse;
+    /// <summary>
+    /// 动物逃离
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormPrecursor_AnimalFlee;
 
-    public static IncidentDef OAGene_AfterSnowstormTraderCaravanArrival; //暴风雪后的商队
-    public static IncidentDef OAGene_SnowstormSurvivorJoins; //风雪后的幸存者
+    /// <summary>
+    /// 暴风雪后的商队
+    /// </summary>
+    public static IncidentDef OAGene_AfterSnowstormTraderCaravanArrival;
+    /// <summary>
+    /// 风雪后的幸存者
+    /// </summary>
+    public static IncidentDef OAGene_SnowstormSurvivorJoins;
     static Snowstorm_IncidentDefOf()
     {
         DefOfHelper.EnsureInitializedInCtor(typeof(Snowstorm_IncidentDefOf));
@@ -142,8 +327,14 @@ public static class Snowstorm_IncidentDefOf
 [DefOf]
 public static class Snowstorm_RimWorldDefOf
 {
-    public static PawnKindDef Husky; //哈士奇
-    public static RaidStrategyDef ImmediateAttackBreaching; //立即破墙
+    /// <summary>
+    /// 哈士奇
+    /// </summary>
+    public static PawnKindDef Husky;
+    /// <summary>
+    /// 立即破墙
+    /// </summary>
+    public static RaidStrategyDef ImmediateAttackBreaching;
     public static TerrainDef BurnedWoodPlankFloor;
 
     public static IncidentCategoryDef AllyAssistance;
