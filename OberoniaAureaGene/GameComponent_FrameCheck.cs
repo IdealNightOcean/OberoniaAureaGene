47f07de45c84ad87facc7e0f1fcfc5648748f647
IdealNightOcean <idealyehai@qq.com>
2026-04-25 19:36:45 +0800

    26.04.25 feat: 添加GameComponent_FrameCheck用于检查框架依赖
    添加GameComponent_FrameCheck组件，在游戏启动或加载时检查必要的框架依赖是否激活。如果依赖未激活，则向玩家显示警告信件。

---
+++ OberoniaAureaGene/GameComponent_FrameCheck.cs
@@ -0,0 +1,33 @@
+﻿using RimWorld;
+using Verse;
+
+namespace OberoniaAureaGene;
+
+public class GameComponent_FrameCheck : GameComponent
+{
+    public GameComponent_FrameCheck(Game game) { }
+
+    public override void StartedNewGame()
+    {
+        base.StartedNewGame();
+        GameStart();
+    }
+
+    public override void LoadedGame()
+    {
+        base.LoadedGame();
+        GameStart();
+    }
+
+    private void GameStart()
+    {
+        if (!ModsConfig.IsActive("OARK.OberoniaAurea.Framework") && !ModsConfig.IsActive("OARK.OberoniaAurea.Framework_Steam"))
+        {
+            Find.LetterStack.ReceiveLetter(
+                label: "OARO_LetterLabel_FrameMiss".Translate(),
+                text: "OARO_LetterText_FrameMiss".Translate(),
+                textLetterDef: LetterDefOf.NegativeEvent,
+                delayTicks: 300);
+        }
+    }
+}
